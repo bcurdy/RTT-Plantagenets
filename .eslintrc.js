5978038b184eb8eeb815bd7fdc57965a657614a7
Tor Andersson <tor@ccxvii.net>
2024-04-18 12:44:24 +0200

    Add typescript configuration to eslint and package.json
    To setup environment:
      npm install
    
    To develop, run this in a separate terminal:
      npm run dev

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 module.exports = {
+	parser: "@typescript-eslint/parser",
+	extends: "eslint:recommended",
 	env: {
 		browser: true,
 		commonjs: true,
 		es2021: true,
-	},
-	extends: "eslint:recommended",
-	parserOptions: {
-		ecmaVersion: "latest",
 	},
 	rules: {
 		"no-constant-binary-expression": "error",
 		indent: [ "warn", "tab", { SwitchCase: 1 } ],
 		semi: [ "error", "never" ],
 		"no-unused-vars": [ "error", { "vars": "all", "args": "all", "argsIgnorePattern": "^_" } ],
+		"prefer-const": 0,
 	},
 }
